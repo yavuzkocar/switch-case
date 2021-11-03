69bf368c3544a8acd14a776ccea73521f2731285
yavuzkocar <87602474+yavuzkocar@users.noreply.github.com>
2021-11-03 16:20:13 +0300

    switch case ilk commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mounth = DateTime.Now.Month;
+            switch (mounth)
+            {
+                case 1:
+                Console.WriteLine("ocak ayındasınız");
+                break;
+                case 2:
+                Console.WriteLine("şubat ayındasınız"); 
+                break;
+                case 3:
+                Console.WriteLine("mart ayındasınız");
+                break;
+                case 4:
+                Console.WriteLine("nisan ayındasınız");
+                break;
+                
+                default:
+                Console.WriteLine("hatalı veri girişi");
+                break;
+                
+            }
+        }
+    }
+}
